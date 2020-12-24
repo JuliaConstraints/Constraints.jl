92c5310c4e9f672df4ddea4c2b580454e967f008
Jean-Francois Baffier <jf@baffier.fr>
2020-12-24 13:35:40 +0900

    Docs (#2)
    * Updated make.jl to point at the correct repo
    
    * Basic doc structure

--- docs/make.jl
+++ docs/make.jl
@@ -7,15 +7,18 @@ makedocs(;
     repo="https://github.com/JuliaConstraints/Constraints.jl/blob/{commit}{path}#L{line}",
     sitename="Constraints.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
+        prettyurls=get(ENV, "CI", nothing) == "true",
         canonical="https://JuliaConstraints.github.io/Constraints.jl",
-        assets=String[],
+        assets = ["assets/favicon.ico"; "assets/github_buttons.js"; "assets/custom.css"],
     ),
     pages=[
         "Home" => "index.md",
+        "Library" => "library.md",
+        "Contributing" => "contributing.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/JuliaConstraints/Constraints.jl",
+    repo="github.com/JuliaConstraints/Constraints.jl.git",
+    devbranch="main",
 )
---
+++ docs/src/library.md
@@ -0,0 +1,8 @@
+# List of constraints
+
+```@index
+```
+
+```@autodocs
+Modules = [Constraints]
+```
