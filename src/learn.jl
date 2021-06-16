03f4950eaf91ff30cab6095fa5619128dce035f1
Azzaare <jf@baffier.fr>
2021-06-16 16:34:09 +0900

    Update learn.jl. Tag new version.

--- src/learn.jl
+++ src/learn.jl
@@ -1,51 +1,56 @@
-function learn_from_icn()
-    targets = Dict(
-        :all_different => Dict(
-            :domains => [domain(1:4) for i in 1:4],
-        ),
-        :all_equal => Dict(
-            :domains => [domain(1:4) for i in 1:4],
-        ),
-        :all_equal_param => Dict(
-            :domains => [domain(8:12) for i in 1:4],
-            :param => 10,
-        ),
-        :dist_different => Dict(
-            :domains => [domain(1:4) for i in 1:4],
-        ),
-        :eq => Dict(
-            :domains => [domain(1:10) for i in 1:2],
-        ),
-        :less_than_param => Dict(
-            :domains => [domain(0:4) for i in 1:1],
-            :param => 2,
-        ),
-        :minus_equal_param => Dict(
-            :domains => [domain(0:4) for i in 1:2],
-            :param => 3,
-        ),
-        :ordered => Dict(
-            :domains => [domain(1:4) for i in 1:4],
-        ),
-        :sequential_tasks => Dict(
-            :domains => [domain(1:4) for i in 1:4],
-        ),
-        :sum_equal_param => Dict(
-            :domains => [domain(1:9) for i in 1:3],
-            :param => 15,
-        ),
+const DEFAULT_TARGETS = Dict(
+    :all_different => Dict(
+        :domains => [domain(1:4) for i in 1:4],
+    ),
+    :all_equal => Dict(
+        :domains => [domain(1:4) for i in 1:4],
+    ),
+    :all_equal_param => Dict(
+        :domains => [domain(8:12) for i in 1:4],
+        :param => 10,
+    ),
+    :dist_different => Dict(
+        :domains => [domain(1:4) for i in 1:4],
+    ),
+    :eq => Dict(
+        :domains => [domain(1:10) for i in 1:2],
+    ),
+    :less_than_param => Dict(
+        :domains => [domain(0:4) for i in 1:1],
+        :param => 2,
+    ),
+    :minus_equal_param => Dict(
+        :domains => [domain(0:4) for i in 1:2],
+        :param => 3,
+    ),
+    :ordered => Dict(
+        :domains => [domain(1:4) for i in 1:4],
+    ),
+    :sequential_tasks => Dict(
+        :domains => [domain(1:4) for i in 1:4],
+    ),
+    :sum_equal_param => Dict(
+        :domains => [domain(1:9) for i in 1:3],
+        :param => 15,
+    ),
+)
+
+const DEFAULT_CONFIG = Dict(
+    :lang => :Julia,
+    :local_iter => 100,
+    :global_iter => 100,
+    :search => :complete,
+    :metric => hamming,
+    :population => 400,
+)
+
+const DEFAULT_PATH = joinpath(dirname(pathof(Constraints)), "compositions")
+
+function learn_from_icn(;
+    targets = DEFAULT_TARGETS,
+    config = DEFAULT_CONFIG,
+    path = DEFAULT_PATH,
     )
-
-    config = Dict(
-        :local_iter => 100,
-        :global_iter => 100,
-        :search => :complete,
-        :metric => hamming,
-        :population => 400,
-    )
-
-    path = joinpath(dirname(pathof(Constraints)),"compositions")
-
     for t in targets
         @info "Starting learning for $(t.first)"
         name = "icn_$(t.first)"
@@ -60,7 +65,7 @@ function learn_from_icn()
             search=config[:search],
             metric=config[:metric],
             popSize=config[:population],
-            language=:Julia,
+            language=config[:lang],
         )
     end
 
