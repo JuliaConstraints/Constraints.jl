281c551bbb80bc777469b64cbb8256a4e47ae11e
Azzaare <jf@baffier.fr>
2021-07-02 15:16:34 +0900

    Invalid composition: sum_equal_param

--- src/compositions/icn_all_equal_param.jl
+++
@@ -1,3 +0,0 @@
-function icn_all_equal_param(x; param=nothing, dom_size)
-    fill(x, 2) .|> map(f -> (y -> f(y; param=param)), [CompositionalNetworks.tr_param_minus_val, CompositionalNetworks.tr_val_minus_param]) |> CompositionalNetworks.ar_sum |> CompositionalNetworks.ag_count_positive |> (y -> CompositionalNetworks.co_identity(y; param=param, dom_size=dom_size, nvars=length(x)))
-end
