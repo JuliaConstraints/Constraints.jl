3cbfe9e130120302e22c5a5238fdc972c7438f07
Azzaare <jf@baffier.fr>
2021-07-17 19:32:52 +0900

    New icn compositions

--- src/compositions/icn_all_equal.jl
+++ src/compositions/icn_all_equal.jl
@@ -1,6 +1,5 @@
 function icn_all_equal(x; X = zeros(length(x), 3), param=nothing, dom_size)
-    fill!(@view(X[1:length(x), :]), 0.0)
-    CompositionalNetworks.tr_in(Tuple([CompositionalNetworks.tr_contiguous_vals_minus_rev, CompositionalNetworks.tr_count_g_left, CompositionalNetworks.tr_count_lesser]), X, x, param)
+    CompositionalNetworks.tr_in(Tuple([CompositionalNetworks.tr_contiguous_vals_minus, CompositionalNetworks.tr_count_l_left, CompositionalNetworks.tr_count_greater]), X, x, param)
     for i in 1:length(x)
         X[i,1] = CompositionalNetworks.ar_sum(@view X[i,:])
     end
