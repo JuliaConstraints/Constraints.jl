f2359915247e042ee48fbfdbbbcc9cc78bb3ab3f
Azzaare <jf@baffier.fr>
2021-07-17 19:32:52 +0900

    New icn compositions

--- src/compositions/icn_all_equal_param.jl
+++ src/compositions/icn_all_equal_param.jl
@@ -1,5 +1,4 @@
 function icn_all_equal_param(x; X = zeros(length(x), 2), param=nothing, dom_size)
-    fill!(@view(X[1:length(x), :]), 0.0)
     CompositionalNetworks.tr_in(Tuple([CompositionalNetworks.tr_param_minus_val, CompositionalNetworks.tr_val_minus_param]), X, x, param)
     for i in 1:length(x)
         X[i,1] = CompositionalNetworks.ar_sum(@view X[i,:])
