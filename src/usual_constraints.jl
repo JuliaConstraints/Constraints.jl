71dc51e05361d44fb3d8105adbaae9f81f4a02ba
Azzaare <jf@baffier.fr>
2023-02-11 16:44:53 +0900

    Move tests to Test Items

--- src/usual_constraints.jl
+++ src/usual_constraints.jl
@@ -78,3 +78,26 @@ macro usual(ex::Expr)
 
     return nothing
 end
+
+## SECTION - Test Items
+@testitem "Usual constraints" tags = [:usual, :constraints] default_imports=false begin
+    using ConstraintDomains
+    using Constraints
+    using Test
+
+    domains = fill(domain(0:3), 4)
+    dom_bool = fill(domain([false, true]), 4)
+
+    for (name, c) in USUAL_CONSTRAINTS
+        for _ in 1:10000
+            for (id, method_params) in enumerate(extract_parameters(c.concept))
+                params = Dict(
+                    map(p -> (p => rand(generate_parameters(domains, p))), method_params)
+                )
+                x = rand(occursin("Bool", Base.arg_decl_parts(methods(c.concept)[id])[2][2][2]) ? dom_bool : domains)
+                @test (c.error(x; params...) > 0.0) != c.concept(x; params...)
+            end
+        end
+        symmetries(c)
+    end
+end
