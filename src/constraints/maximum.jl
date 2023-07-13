1eda8a0fb5a691b9de6218a8c2194aee47f357f7
Azzaare <jf@baffier.fr>
2023-07-13 01:43:07 +0000

    Tests for intention maximum mdd minimum

--- src/constraints/maximum.jl
+++ src/constraints/maximum.jl
@@ -3,3 +3,15 @@ xcsp_maximum(; list, condition) = condition[1](maximum(list), condition[2])
 const description_maximum = """Global constraint ensuring that all ...`"""
 
 @usual concept_maximum(x; op = ==, val) = xcsp_maximum(; list = x, condition = (op, val))
+
+@testitem "Maximum" tags = [:usual, :constraints, :maximum] begin
+    c = USUAL_CONSTRAINTS[:maximum] |> concept
+    e = USUAL_CONSTRAINTS[:maximum] |> error_f
+    vs = Constraints.concept_vs_error
+
+    @test c([1, 2, 3, 4, 5]; op = ==, val = 5)
+    @test !c([1, 2, 3, 4, 5]; op = ==, val = 6)
+
+    @test vs(c, e, [1, 2, 3, 4, 5]; op = ==, val = 5)
+    @test vs(c, e, [1, 2, 3, 4, 5]; op = ==, val = 6)
+end
