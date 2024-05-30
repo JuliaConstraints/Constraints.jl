372696a85c5778e1409d9eb99251a4bc088bd71f
Azzaare <jf@baffier.fr>
2024-05-30 14:45:40 +0900

    Spelling

--- src/constraint.jl
+++ src/constraint.jl
@@ -6,9 +6,9 @@ const USUAL_SYMMETRIES = Dict(:permutable => sort)
 
 """
     Constraint
-Parametric stucture with the following fields.
+Parametric structure with the following fields.
 - `concept`: a Boolean function that, given an assignment `x`, outputs `true` if `x` satisfies the constraint, and `false` otherwise.
-- `error`: a positive function that works as preferences over invalid assignements. Return `0.0` if the constraint is satisfied, and a strictly positive real otherwise.
+- `error`: a positive function that works as preferences over invalid assignments. Return `0.0` if the constraint is satisfied, and a strictly positive real otherwise.
 """
 mutable struct Constraint{FConcept <: Function, FError <: Function}
     args::Int
